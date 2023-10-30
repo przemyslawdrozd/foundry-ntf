ced76d32c0d8900ff05ca69e6fadc280e2762c2f
Przemo <przemo06133@gmail.com>
2023-10-30 15:48:06 +0100

    Check mint and balance of NFT

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -9,6 +9,10 @@ contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
 
+    address public _przemo = makeAddr("Przemo");
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
     function setUp() public {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
@@ -34,5 +38,16 @@ contract BasicNftTest is Test {
     function testSymbolIsCorrect() public {
         string memory givenSymbol = basicNft.symbol();
         assertEq(givenSymbol, "DOG");
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(_przemo);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(_przemo) == 1);
+        assert(
+            keccak256(abi.encodePacked(PUG)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
     }
 }
